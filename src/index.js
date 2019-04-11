ebec2b55cc2ec4a063b71d96d80acfef631fc64e
hjPark <cool.itea@gmail.com>
2019-04-11 19:12:22 +0900

    IE 지원

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import Root from './Root';
 import * as serviceWorker from './serviceWorker';
+import 'react-app-polyfill/ie9'; // For IE 9-11 support
+import 'react-app-polyfill/ie11'; // For IE 11 support
 
 ReactDOM.render(<Root />, document.getElementById('root'));
 
