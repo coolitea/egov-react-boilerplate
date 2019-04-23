bec10b7cf4ce3fb78163438dcc60e1ae6c6d3674
hjPark <cool.itea@gmail.com>
2019-04-23 21:19:06 +0900

    .env 추가

--- src/components/store/index.js
+++ src/components/store/index.js
@@ -1,10 +1,13 @@
-import { createStore, compose, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
+import { createStore, compose, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
 export default function configureStore(reducer, initialState = {}) {
   const enhancer = compose(
     applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    window.devToolsExtension
+      ? window.__REDUX_DEVTOOLS_EXTENSION__ &&
+          window.__REDUX_DEVTOOLS_EXTENSION__()
+      : f => f
     // window.devToolsExtension ? window.devToolsExtension() : f => f
   );
   return createStore(reducer, initialState, enhancer);
